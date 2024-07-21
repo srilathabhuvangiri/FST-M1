6ba2529dea1e3a0f52623fc48b90c4ceedc1b203
srilathabhuvangiri <srilatha.bhuvangiri@ibm.com>
2024-07-21 21:29:21 +0530

    Update Activity4.java

--- Appium/Activities/Activity4.java
+++ Appium/Activities/Activity4.java
@@ -1,75 +1,50 @@
-package activities;
 import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
 
 public class Activity4 {
-    // Driver Declaration
     AndroidDriver driver;
     WebDriverWait wait;
 
-    // Set up method
     @BeforeClass
-    public void setUp() throws MalformedURLException {
-        // Desired Capabilities
-        UiAutomator2Options options = new UiAutomator2Options();
+    public void setup() throws MalformedURLException {
+        UiAutomator2Options options= new UiAutomator2Options();
         options.setPlatformName("android");
         options.setAutomationName("UiAutomator2");
-        options.setAppPackage("com.android.contacts");
-        options.setAppActivity(".activities.PeopleActivity");
+        options.setAppPackage("com.google.android.contacts");
+        options.setAppActivity("com.android.contacts.activities.PeopleActivity");
         options.noReset();
 
-        // Server Address
-        URL serverURL = new URL("http://localhost:4723/");
+        URL serverURL= new URL("http://localhost:4723/");
 
-        // Driver Initialization
-        driver = new AndroidDriver(serverURL, options);
-        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        driver= new AndroidDriver(serverURL, options);
+        wait= new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
-    // Test method
     @Test
-    public void contactsTest() {
-        // Find and click the add button
-        driver.findElement(AppiumBy.accessibilityId("Create new contact")).click();
-
-        // Wait for elements to load
-        wait.until(ExpectedConditions.elementToBeClickable(
-                AppiumBy.xpath("//android.widget.EditText[@text='First name']")
-        ));
-
-        // Enter the details
-        driver.findElement(AppiumBy.xpath(
-                "//android.widget.EditText[@text='First name']"
-        )).sendKeys("srilatha");
-        driver.findElement(AppiumBy.xpath(
-                "//android.widget.EditText[@text='Last name']"
-        )).sendKeys("bhuvangiri");
-        driver.findElement(AppiumBy.xpath(
-                "//android.widget.EditText[@text='Phone']"
-        )).sendKeys("953454343");
-        // Click Save
-        driver.findElement(AppiumBy.id("editor_menu_save_button")).click();
-
-        // Wait for contact to save
-        wait.until(ExpectedConditions.elementToBeClickable(AppiumBy.id("large_title")));
-
-        // Assertion
-        String contactName = driver.findElement(AppiumBy.id("large_title")).getText();
-        Assert.assertEquals(contactName, "srilatha bhuvangiri");
+    public void contactsTest(){
+        driver.findElement(AppiumBy.accessibilityId("Create contact")).click();
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='First name']")).sendKeys("Aaditya");
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Last name']")).sendKeys("Varma");
+        driver.findElement(AppiumBy.xpath("//android.widget.EditText[@text='Phone']")).sendKeys("999148292");
+        driver.findElement(AppiumBy.id("toolbar_button")).click();
+        wait.until(ExpectedConditions.presenceOfElementLocated(AppiumBy.id("com.google.android.contacts:id/large_title")));
+        String savedContact= driver.findElement(AppiumBy.id("com.google.android.contacts:id/large_title")).getText();
+        Assert.assertEquals(savedContact, "Aaditya Varma");
     }
 
-    // Tear down method
     @AfterClass
-    public void tearDown() {
-        // Close the app
+    public void tearDown(){
         driver.quit();
     }
 }
